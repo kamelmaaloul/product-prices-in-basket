4f216497e41c622a8faac91027aeedeb375c2054
kamelmaaloul <68155541+kamelmaaloul@users.noreply.github.com>
2021-08-13 14:34:59 +0100

    product-prices-in-basket

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -4,6 +4,7 @@ import {map} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiService {
 
   constructor(private http : HttpClient) { }
@@ -15,3 +16,8 @@ export class ApiService {
     }))
   }
 }
+//fakeStoreApi est une API REST en ligne gratuite que vous pouvez utiliser 
+//chaque fois que vous avez besoin de données pseudo-réelles pour votre site
+ //de commerce électronique ou d'achat sans exécuter de code côté serveur.
+ // C'est génial pour l'enseignement, les exemples de codes, les tests, etc.
+
